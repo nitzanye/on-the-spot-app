2b48114442d90b2e08dd8b9c7c916b1cf60ec314
NitzanSmulevici <nitzanye@gmail.com>
2023-03-10 02:08:59 +0200

    fix image type

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { getUsers } from "@/prisma/users";
 
 const Users = async () => {
-  const data: {id: string, email: string, name: string, imageUrl: string}[] = await getUsers();
+  const data: {id: string, email: string, name: string, image: string}[] = await getUsers();
   // console.log("data", data)
   return (
     <main className="flex justify-center flex-col pt-10">
@@ -11,7 +11,7 @@ const Users = async () => {
           <div className='flex justify-center flex-col ml-auto mr-auto pb-10' key={user.id}>
           <h3>{user.name}</h3>
           <h4>{user.email}</h4>
-          <img className='max-w-sm' src={user.imageUrl} />
+          <img className='max-w-sm' src={user.image} />
           </div>
         ))}
       <Link className="bg-[#09312c48] text-white font-medium py-4 px-2 rounded-md  w-64 text-center ml-auto mr-auto"
