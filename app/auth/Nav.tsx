1688a82a025939d690c01e26c69fcd56cde72a90
NitzanSmulevici <nitzanye@gmail.com>
2023-03-10 11:41:52 +0200

    fix Nav margins

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -9,12 +9,12 @@ const session = await getServerSession(authOptions);
 console.log(session)
 
   return (
-    <div className='bg-stone-100 py-10'>
-      <nav className='container flex  text-sm font-medium tracking-wider uppercase text-stone-500'>
-        <Link  className='items-center py-5' href={'/'}>
-          <h1 className=" flex ml-auto items-center justify-center font-bold text-lg ">Send it.</h1>
+    <div className='bg-stone-100 py-10 '>
+      <nav className='flex justify-around items-center text-sm font-medium tracking-wider uppercase text-stone-500'>
+        <Link  className='py-5' href={'/'}>
+          <h1 className="flex items-center  font-bold text-lg ">Send it.</h1>
         </Link>
-        <ul className='ml-auto flex  items-center gap-6'></ul>
+        <ul className='justify-center flex items-center gap-6'></ul>
           {!session?.user && <Login />}
           {session?.user && <Logged image={session.user.image || ''}/>}
       </nav>
